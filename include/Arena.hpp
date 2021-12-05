457d770a546d31ff4675efd577d54fea92e0e349
Jan Rádl <Honzar24@gmail.com>
2021-12-05 23:34:00 +0100

    Arena and main

--- include/Arena.hpp
+++ include/Arena.hpp
@@ -1,11 +1,17 @@
 #pragma once
+#include <cassert>
 
-#include <cstddef>
+#include <vector>
+#include <ranges>
+#include <functional>
+#include <algorithm>
+
+#include <P_types.hpp>
+#include <Object.hpp>
 
 class Arena
 {
-    using pos_t = size_t;
-
+    using  TimedObject = std::tuple<size_t,size_t,Object>;
 public:
     enum class Flags
     {
@@ -14,11 +20,75 @@ public:
         REPORT_DISTANCE
     };
 
-    Arena(pos_t w, pos_t h):width(w),height(h){}
+    Arena(pos_type w, pos_type h,time_type stepSize,size_t n_steps):width(w),height(h),step_size(stepSize),step_count(0),max_step_count(n_steps){}
+    Arena(pos_type w, pos_type h,size_t n_steps):Arena(w,h,1,n_steps){};
+    Arena():Arena(100,100,100){};
+
+    ~Arena() = default;
+
+    void add(size_t start,size_t end, Object& o)
+    {
+        add(start,end,std::move(o));
+    }
+
+    void add(size_t start,size_t end, Object&& o)
+    {
+        assert(start < end);
+        objects.push_back({start,end,o});
+    }
+
+    void step()
+    {
+        if(end())
+        {
+            return;
+        }
+        step_count++;
+        removeEnded();
+        update();
+        
+
+    }
+
+    bool end()
+    {
+        return step_count >= max_step_count;
+    }
 
     private:
-    pos_t width = 100;
-    pos_t height = 100;
-    double stepsize = 0.1;
+    pos_type width;
+    pos_type height;
+    time_type step_size;
+    size_t step_count;
+    size_t max_step_count;
+    std::vector<TimedObject> objects;
+    std::vector<TimedObject> endeds;
+
+    void removeEnded()
+    {
+        auto ended = [&](auto i) -> bool{ 
+            auto&[start,end,o] = i;
+            return end <= step_count;};
+        for(auto it = objects.begin(); it != objects.end();)
+        {
+            auto fits = std::find_if(it,objects.end(),ended);
+            if(fits != std::end(objects))
+            {
+                auto fite = std::find_if_not(fits,objects.end(),ended);
+                std::move(fits,fite,std::back_inserter(endeds));
+                it  = objects.erase(fits,fite);
+            }
+            it = fits;
+        }
+    }
+
+    void update()
+    {
+        auto current_objects = objects | std::views::filter([&](auto i) {return std::get<0>(i) <= step_count;});
+        for(auto&[start,end,o]:current_objects)
+        {
+            o.update(step_size);
+        }
+    }
 
 };
--- main.cpp
+++ main.cpp
@@ -5,7 +5,14 @@
 
 int main(int argc, char** argv) {
 
-    Arena arena(200,200);
+    Arena arena(200,200,100);
+    arena.add(0,100,Object("0100",0,0,10,10));
+    arena.add(0,2,Object("02",0,0,10,10));
+    arena.add(5,10,Object("510",0,0,10,10));
+    while(!arena.end())
+    {
+        arena.step();
+    }
     std::cout << "Hello, world!\n";
     return 0;
 }
