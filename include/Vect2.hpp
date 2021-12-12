c2cc4284284913ae2fcfa294c08efb3fa986f39a
Jan Rádl <Honzar24@gmail.com>
2021-12-12 16:02:48 +0100

    Vect2 added lenght and norm

--- include/Vect2.hpp
+++ include/Vect2.hpp
@@ -2,6 +2,7 @@
 
 #include <P_types.hpp>
 #include <tuple>
+#include <cmath>
 
 class Vect2
 {
@@ -9,11 +10,17 @@ public:
     pos_type x;
     pos_type y;
 
-    Vect2(pos_type x, pos_type y) : x(x), y(y) {};
+    Vect2(const pos_type x, const pos_type y) : x(x), y(y) {};
     Vect2() : Vect2(0, 0) {};
 
     ~Vect2() = default;
 
+    inline Vect2& operator+(const Vect2& ov)
+    {
+        x += ov.x;
+        y += ov.y;
+        return *this;
+    }
     inline Vect2& operator+=(Vect2& ov)
     {
         x += ov.x;
@@ -26,14 +33,32 @@ public:
         y *= s;
         return *this;
     }
-    inline bool operator==(Vect2 o)
+    inline bool operator==(Vect2& o) const
     {
         return std::tie(x, y) == std::tie(o.x, o.y);
     }
+
+    inline pos_type lenght()
+    {
+        return std::sqrt(x * x + y * y);
+    }
+
+    inline Vect2 norm()
+    {
+        pos_type l = lenght();
+        if (lenght() == 0)
+        {
+            return *this;
+        }
+        x /= l;
+        y /= l;
+        return *this;
+    }
+
+    inline friend std::ostream& operator<<(std::ostream& out, Vect2& v)
+    {
+        out << "[" << v.x << "," << v.y << "]";
+        return out;
+    }
 };
 
-inline std::ostream& operator<<(std::ostream& out, Vect2& v)
-{
-    out << "[" << v.x << "," << v.y << "]";
-    return out;
-}
